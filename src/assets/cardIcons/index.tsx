b8f36c98d3252ecfa290ba56c16e595e6eef632a
Rafael Carrenho <rafael.carrenho@hotmail.com>
2022-06-20 16:49:57 -0300

    feat: Add card icons

---
+++ src/assets/cardIcons/index.tsx
@@ -0,0 +1,10 @@
+export const icons = {
+  book: require("./book.png"),
+  star: require("./star.png"),
+  gift: require("./gift.png"),
+  idea: require("./idea.png"),
+  like: require("./like.png"),
+  map: require("./map.png"),
+  money: require("./money.png"),
+  pencil: require("./pencil.png"),
+};
